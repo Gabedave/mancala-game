288c201a404c2d0f98b67045c6f7eb886569f24c
David Gabriel <gabedave01@gmail.com>
2023-11-01 00:28:27 +0100

    add dockerfile

---
+++ src/main/java/com/gabedave/mancalagame/controllers/HealthController.java
@@ -0,0 +1,19 @@
+package com.gabedave.mancalagame.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public ResponseEntity<Void> healthCheck() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+}
