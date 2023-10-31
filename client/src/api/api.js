cb780127bb4ed405653053a092004df79f3c6f11
David Gabriel <gabedave01@gmail.com>
2023-10-31 22:22:54 +0100

    add api_url to env

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,6 @@
 export default class Api {
-  static API_URL = "http://localhost:8080/api/v1";
+  static API_URL =
+    process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1";
 
   static getActiveGames = async () => {
     try {
